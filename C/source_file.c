6d22a943453b2de87bf5520eecec3d1588ed5311
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-06-14 16:53:16 +0300

    Course Updates

--- C/source_file.c
+++ C/source_file.c
@@ -1,8 +1,34 @@
-//
-// Created by SurfacePro on 16/05/2015.
-//
 #include <stdio.h>
+unsigned int reverseBits(unsigned int num)
+{
+    unsigned int  NO_OF_BITS = sizeof(num) * 8;
+    unsigned int reverse_num = 0, i, temp;
 
-int main(){
-    printf("...");
+    for (i = 0; i < NO_OF_BITS; i++)
+    {
+        temp = (num & (1 << i));
+        if(temp)
+            reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
+    }
+
+    return reverse_num;
 }
+
+/* Driver function to test above function */
+int main()
+{
+    unsigned int x = 2;
+    printf("%u", reverseBits(x));
+    getchar();
+}
+
+
+
+
+
+
+
+
+
+
+
